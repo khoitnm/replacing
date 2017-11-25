7f6bc9d862e9f38373470909bd021c39cfbd8af1
Khoi Tran <khoi.tnm@gmail.com>
2017-11-26 02:04:24 +0800

    Changing color from light to dark: not completed.

--- src/main/java/org/tnmk/replacing/all/color/ColorUtils.java
+++ src/main/java/org/tnmk/replacing/all/color/ColorUtils.java
@@ -1,14 +1,55 @@
 package org.tnmk.replacing.all.color;
 
+import java.awt.*;
+
 public final class ColorUtils {
-    private ColorUtils(){
+    private ColorUtils() {
         //Utils
     }
 
-    public int toRgb(int red, int blue, int green){
+    public int toRgb(int red, int blue, int green) {
         int rgb = red;
         rgb = (rgb << 8) + green;
         rgb = (rgb << 8) + blue;
         return rgb;
+    }
+
+    public static String hsvToRgb(float hue, float saturation, float value) {
+
+        int h = (int) (hue * 6);
+        float f = hue * 6 - h;
+        float p = value * (1 - saturation);
+        float q = value * (1 - f * saturation);
+        float t = value * (1 - (1 - f) * saturation);
+
+        switch (h) {
+            case 0:
+                return rgbToString(value, t, p);
+            case 1:
+                return rgbToString(q, value, p);
+            case 2:
+                return rgbToString(p, value, t);
+            case 3:
+                return rgbToString(p, q, value);
+            case 4:
+                return rgbToString(t, p, value);
+            case 5:
+                return rgbToString(value, p, q);
+            default:
+                throw new RuntimeException("Something went wrong when converting from HSV to RGB. Input was " + hue + ", " + saturation + ", " + value);
+        }
+    }
+
+    public static float[] rbgToHsb(int r, int g, int b) {
+        float[] hsb = new float[3];
+        Color.RGBtoHSB(r, g, b, hsb);
+        return hsb;
+    }
+
+    public static String rgbToString(float r, float g, float b) {
+        String rs = Integer.toHexString((int) (r * 256));
+        String gs = Integer.toHexString((int) (g * 256));
+        String bs = Integer.toHexString((int) (b * 256));
+        return rs + gs + bs;
     }
 }
