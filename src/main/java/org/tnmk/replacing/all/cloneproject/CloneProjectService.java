b86ef14fc3befeed5b0598cb455c64c05f36bbb9
Khoi Tran <khoi.tnm@gmail.com>
2019-02-23 19:21:09 -0500

    Fixbug: CloneProjectService: fixes error when originalName start with "\\" or "/"

--- src/main/java/org/tnmk/replacing/all/cloneproject/CloneProjectService.java
+++ src/main/java/org/tnmk/replacing/all/cloneproject/CloneProjectService.java
@@ -15,16 +15,15 @@ import java.util.Map;
 @Service
 public class CloneProjectService {
     public static final List<String> PATTERN_EXCLUDING_JAVA_PROJECT = Arrays.asList(
-            ".*[\\/]\\.git",
-            ".*[\\/]\\.idea",
-            ".*[\\/]\\.gradle",
-            ".*\\.class");
+        ".*[\\/]\\.git",
+        ".*[\\/]\\.idea",
+        ".*[\\/]\\.gradle",
+        ".*\\.class");
     @Autowired
     private CopyingAndReplacingService copyingAndReplacingService;
 
 
     /**
-     *
      * @param sourcePath
      * @param newSingularName words should be separated by spaces or hyphen. Each word could be lowercase, uppercase, capitalized,
      *                        For example: "the-name-02"
@@ -33,14 +32,17 @@ public class CloneProjectService {
         int lastPathIndex = sourcePath.lastIndexOf('/');
         lastPathIndex = Math.max(lastPathIndex, sourcePath.lastIndexOf('\\'));
         if (lastPathIndex <= 0) {
-            throw new RuntimeException("The sourcePath "+sourcePath+" must have at least one path indication character (e.g. '/' or '\\'");
+            throw new RuntimeException("The sourcePath " + sourcePath + " must have at least one path indication character (e.g. '/' or '\\'");
         }
 
         String oldSingularName = sourcePath.substring(lastPathIndex);
+        if (oldSingularName.startsWith("\\") || oldSingularName.startsWith("/")) {
+            oldSingularName = oldSingularName.substring(1);
+        }
         simpleCloneToTheSameParentFolder(sourcePath, oldSingularName, newSingularName);
     }
+
     /**
-     *
      * @param sourcePath
      * @param oldSingularName words should be separated by spaces or hyphen. Each word could be lowercase, uppercase, capitalized.
      *                        For example: "the-name-01"
@@ -51,13 +53,11 @@ public class CloneProjectService {
         List<String> excludingPatterns = PATTERN_EXCLUDING_JAVA_PROJECT;
         String destPath = sourcePath;//Clone to the same folder.
         File parentFolder = IOUtils.createParentFolderIfNecessary(destPath);
-        destPath = parentFolder.getAbsolutePath()+"/"+new LowerCaseWithHyphenTransformer().apply(newSingularName);
+        destPath = parentFolder.getAbsolutePath() + "/" + new LowerCaseWithHyphenTransformer().apply(newSingularName);
 
         Map<String, String> renaming = MultiFormRenameHelper.createMultiFormRenameMap(oldSingularName, newSingularName);
         this.copyingAndReplacingService.copyingAndReplacing(sourcePath, destPath, excludingPatterns, renaming);
     }
-
-
 
 
 }
