5bf66a9cadcc4d1f8653c0b002f612e8b85d7caf
khoi <khoi.tnm@gmail.com>
2019-11-30 18:26:14 -0500

    Fix compile.

--- src/main/java/org/tnmk/replacing/all/cloneproject/CloneProjectService.java
+++ src/main/java/org/tnmk/replacing/all/cloneproject/CloneProjectService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.tnmk.replacing.all.common.multiformname.MultiFormRenameHelper;
 import org.tnmk.replacing.all.renaming.CopyingAndReplacingService;
 import org.tnmk.replacing.all.renaming.ReplacingService;
 import org.tnmk.replacing.all.util.IOUtils;
