acba736510d7e5b8f722be38ef6a30c48084f2c1
Khoi Tran <khoi.tnm@gmail.com>
2021-01-04 09:31:33 -0500

    Refactor Dependency model.

--- src/main/java/org/tnmk/replacing/all/checkdependencies/package-info.java
+++ src/main/java/org/tnmk/replacing/all/checkdependencies/package-info.java
@@ -1,5 +1,5 @@
 /**
- * The main class in this package is {@link org.tnmk.replacing.all.checkdependencies.CheckAccessibleDependenciesFromFile}
+ * The main class in this package is {@link org.tnmk.replacing.all.checkdependencies.CheckAccessibleDependenciesFromMvnLogFile}
  * How to use that file: Please view the client code in "CheckAccessibleDependenciesInFileTest"
  */
 package org.tnmk.replacing.all.checkdependencies;
