3e553c6b5c43d639b85d3e9608101e8067376deb
Khoi Tran <khoi.tnm@gmail.com>
2021-01-18 15:09:32 -0500

    Move @Disabled annotation to class level for manual trigger.

--- src/test/java/org/tnmk/replacing/all/common/BaseSpringTest.java
+++ src/test/java/org/tnmk/replacing/all/common/BaseSpringTest.java
@@ -2,10 +2,6 @@ package org.tnmk.replacing.all.common;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@ActiveProfiles("test")
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//@Ignore
 @SpringBootTest
 public abstract class BaseSpringTest {
 }
