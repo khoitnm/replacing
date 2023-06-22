9f3862943461fa3129a5771f0aa65950078f7bf2
trank <Kevin.Tran@pointclickcare.com>
2023-06-21 22:30:38 -0400

    - Fix pom.xml packaging to jar - Upgrade Java 11 - Upgrade spring boot: 2.7.12 - Upgrade common-io 2.13.0 - Upgrade junrar 7.5.4 - Upgrade jarchivelib 1.2.0 - Remove tukaani.xz:1.9

--- src/main/java/org/tnmk/replacing/all/unzip/UnzipService.java
+++ src/main/java/org/tnmk/replacing/all/unzip/UnzipService.java
@@ -1,5 +1,6 @@
 package org.tnmk.replacing.all.unzip;
 
+import com.github.junrar.exception.RarException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.tnmk.replacing.all.util.IOUtils;
 import org.tnmk.replacing.all.util.ZipUtils;
 
 import java.io.File;
+import java.io.IOException;
 
 @Service
 public class UnzipService {
@@ -23,9 +25,13 @@ public class UnzipService {
         this.traverseFolderService.traverseFile(file, currentFile -> {
             String absPath = currentFile.getAbsolutePath();
             File parentFolder = IOUtils.createParentFolderIfNecessary(absPath);
-            if (ZipUtils.isCompressedFileName(currentFile.getName())){
-                String target = ZipUtils.unzip(currentFile.getAbsolutePath(),parentFolder.getAbsolutePath(), true);
-                LOGGER.info("Unzip {} to {}",currentFile.getAbsolutePath(), target);
+            if (ZipUtils.isCompressedFileName(currentFile.getName())) {
+                try {
+                    String target = ZipUtils.unzip(currentFile.getAbsolutePath(), parentFolder.getAbsolutePath(), true);
+                    LOGGER.info("Unzip {} to {}", currentFile.getAbsolutePath(), target);
+                } catch (RarException | IOException e) {
+                    throw new IllegalStateException("Cannot extract zip file: " + currentFile.getAbsolutePath());
+                }
             }
             return currentFile;
         });
